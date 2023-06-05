b9f126f09b804b20ab01eab893bcbd2dff03ba8e
Yousef Al-waer <elwaeryousef@gmail.com>
2023-06-05 12:39:36 +0300

    init the application main structre

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const shortlinkRoutes = require("./routes/shortlinkRoutes");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+app.use(helmet());
+
+// Routes
+app.use("/api/v1/shortlinks", shortlinkRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.statusCode || 500).json({
+    status: err.status || "error",
+    message: err.message || "Internal Server Error",
+  });
+});
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`cant find ${req.originalUrl} on this server!!!`, 404));
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,48 @@
+const dotenv = require("dotenv");
+
+dotenv.config({
+  path: "./config.env",
+});
+
+const mongoose = require("mongoose");
+const app = require("./app");
+
+var DB_URL = process.env.DATABASE_URL.replace(
+  "<USER>",
+  process.env.DATABASE_USER
+);
+DB_URL = DB_URL.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+DB_URL = DB_URL.replace("<HOST>", process.env.DATABASE_HOST);
+
+mongoose
+  .connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to the database:", err);
+  });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.message);
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("uncaughtException", (err) => {
+  console.log(err.message);
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  server.close(() => {
+    process.exit(1);
+  });
+});
