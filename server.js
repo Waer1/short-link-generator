3c4247ff3977d627503662aa0929f7605b758808
Yousef Al-waer <elwaeryousef@gmail.com>
2023-06-05 13:41:12 +0300

    remove the wrong error handle

--- server.js
+++ server.js
@@ -30,19 +30,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
-
-process.on("unhandledRejection", (err) => {
-  console.log(err.message);
-  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
-process.on("uncaughtException", (err) => {
-  console.log(err.message);
-  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
-  server.close(() => {
-    process.exit(1);
-  });
-});
